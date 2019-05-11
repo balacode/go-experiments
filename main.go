7a9f290a346c5eefc283e5de17ce1a5644489a7f
balarabe@protonmail.com <balarabe@protonmail.com>
2019-05-11 14:25:37 +0100

    Clean-up main.go

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2018-06-14 01:17:40 D828E8                       go-experiments/[main.go]
+// :v: 2019-05-11 14:25:17 0D649E                       go-experiments/[main.go]
 // -----------------------------------------------------------------------------
 
 package main
 
 import (
 	"fmt"
-	/// "reflect"
 )
 
 func main() {
 	fmt.Println("running go-experiments")
 
-	/// serverDemo()
-	tlsServerDemo()
-
-	///fmt.Println("type of AddFunc:", reflect.TypeOf(AddFunc))
-	///fmt.Println("type of DeleteFunc:", reflect.TypeOf(DeleteFunc))
-}
-
-type AddFuncT func(a int, b string)
-
-var AddFunc AddFuncT = func(a int, b string) {
-}
-
-type DeleteFuncT func(a int, b string)
-
-var DeleteFunc DeleteFuncT = func(a int, b string) {
+	// serverDemo()
+	// tlsServerDemo()
 }
 
 //end
